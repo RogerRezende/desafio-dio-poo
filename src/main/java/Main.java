40fcd9b490970b8f33c6acb965c07cc0e9282850
roger.santos <roger.rezende@gmail.com>
2022-04-02 13:47:01 -0300

    criação das classes main, curso e mentoria

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        Curso curso2 = new Curso();
+        Mentoria mentoria1 = new Mentoria();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do Curso de Java");
+        curso1.setCargaHoraria(8);
+
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Descrição do Curso de Python");
+        curso2.setCargaHoraria(8);
+
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição da Mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
